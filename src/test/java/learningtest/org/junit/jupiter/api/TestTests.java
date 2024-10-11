17a247d565b94f9527ed947a51c6c8d516664952
Johnny Lim <izeye@naver.com>
2024-10-11 23:50:41 +0900

    Add hello world for JUnit

---
+++ src/test/java/learningtest/org/junit/jupiter/api/TestTests.java
@@ -0,0 +1,17 @@
+package learningtest.org.junit.jupiter.api;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for {@link Test}.
+ *
+ * @author Johnny Lim
+ */
+class TestTests {
+
+    @Test
+    void test() {
+        System.out.println("Hello, JUnit!");
+    }
+
+}
